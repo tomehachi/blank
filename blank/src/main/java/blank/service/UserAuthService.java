049a62fcfd81b8bafc21f3ecd219d3b549272614
tomehachi <tomehachi@gmail.com>
2015-11-01 10:38:05 +0900

    サービス処理追加

--- blank/src/main/java/blank/service/UserAuthService.java
+++ blank/src/main/java/blank/service/UserAuthService.java
@@ -1,22 +1,26 @@
 package blank.service;
 
-import blank.entity.UserAuth;
-import java.util.List;
-import javax.annotation.Generated;
-
 import static blank.entity.UserAuthNames.*;
 import static org.seasar.extension.jdbc.operation.Operations.*;
 
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import org.seasar.extension.jdbc.where.SimpleWhere;
+
+import blank.entity.UserAuth;
+
 /**
  * {@link UserAuth}のサービスクラスです。
- * 
+ *
  */
 @Generated(value = {"S2JDBC-Gen 2.4.46", "org.seasar.extension.jdbc.gen.internal.model.ServiceModelFactoryImpl"}, date = "2015/08/06 13:35:05")
 public class UserAuthService extends AbstractService<UserAuth> {
 
     /**
      * 識別子でエンティティを検索します。
-     * 
+     *
      * @param userId
      *            識別子
      * @return エンティティ
@@ -27,10 +31,25 @@ public class UserAuthService extends AbstractService<UserAuth> {
 
     /**
      * 識別子の昇順ですべてのエンティティを検索します。
-     * 
+     *
      * @return エンティティのリスト
      */
     public List<UserAuth> findAllOrderById() {
         return select().orderBy(asc(userId())).getResultList();
+    }
+
+    /**
+     * メールアドレスをキーに有効なユーザ情報を取得.<br>
+     *
+     * @param mailAddr メールアドレス
+     * @return ユーザ情報
+     */
+    public UserAuth findAvailableUserByMailAddr(String mailAddr) {
+        return jdbcManager.from(UserAuth.class)
+        .where(new SimpleWhere()
+            .eq("mailAddr", mailAddr)
+            .eq("delFlag", false)
+        )
+        .getSingleResult();
     }
 }
